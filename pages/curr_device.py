3440fadd8d90f09027059a3248c3015daedad874
Kristian Jekov <kristian.jekov7@gmail.com>
2024-07-17 17:05:56 +0300

    a

--- pages/curr_device.py
+++ pages/curr_device.py
@@ -2,12 +2,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from utils.loading_funcs import wait_for_element
 from config import config
 
 
 def edit_curr_device(driver):
 
-    time.sleep(0.75)
+    wait_for_element(driver, config.EDIT_DEVICE_VERSION)
 
     edit_device_version_button = driver.find_element(By.XPATH, config.EDIT_DEVICE_VERSION)
     edit_device_version_button.click()
--- utils/loading_funcs.py
+++ utils/loading_funcs.py
@@ -18,7 +18,7 @@ def wait_for_page_load(driver, timeout=10):
        
 def wait_for_element(driver, element):
     while True:
-        time.sleep(0.2)
+        time.sleep(0.1)
         try:
 
             WebDriverWait(driver,timeout=6).until(EC.element_to_be_clickable((By.XPATH, element)))
@@ -26,5 +26,5 @@ def wait_for_element(driver, element):
         
         except:
             print("Waiting for devices to load...")
-            time.sleep(0.2)
+            time.sleep(0.1)
             break
