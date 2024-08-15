1829eeb280831e63d8138399e1b2903b334a5314
Kristian Jekov <kristian.jekov7@gmail.com>
2024-08-15 15:54:26 +0300

    fixed read from thread

--- CONSOLE_LOG/read.py
+++ CONSOLE_LOG/read.py
@@ -1,32 +1,44 @@
 import serial
-
-import time
+import time 
 from filter import check_all_components_vers
 from update_info import check_if_update_available
-import queue as q
 
-def read_all(strg, qq: q.Queue):
+def read_all():
     high = False
     low = True
- 
-    ser = serial.Serial(baudrate=921600)
 
-    ser.port = strg
- 
- 
+    ser = serial.Serial(baudrate=921600)
+    ser.port = "COM17"
+
+
     ser.dtr = low
+    ser.rts = high
+    ser.dtr = ser.dtr
+    ser.open()
+
+    ser.dtr = high
+    ser.rts = low
+    ser.dtr = ser.dtr
+
+    time.sleep(0.005)
+
+    ser.rts = high
+    ser.dtr = ser.dtr
 
     line = ser.readline()
     device_ver_dict = {}
- 
+
+
 
     while True:
         line = ser.readline().decode('utf-8').rstrip()
-        qq.put(item=line, block=False)
+        # print(line)
+        if check_if_update_available(line):   
+            break
+        # TODO fix the bug for the secont main loop and etc
+        #check_all_components_vers(line,device_ver_dict)
 
-       # print(str(line))
-       # if check_if_update_available(line):
-        #     break
-         # TODO fix the bug for the secont main loop and etc
-         #check_all_components_vers(line,device_ver_dict)
- 
+        
+
+    
+    
